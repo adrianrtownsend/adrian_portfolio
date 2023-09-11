c59a05837ffb9120311da9f0e4286c88b93409e3
Adrian Townsend <greenlight8004@gmail.com>
2023-09-11 12:16:59 -0400

    fix: fixed typescript errors

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,7 +8,7 @@ import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
 const Contact = () => {
-	const formRef = useRef();
+	const formRef = useRef(null);
 	const [form, setForm] = useState({
 		name: '',
 		email: '',
@@ -17,9 +17,10 @@ const Contact = () => {
 
 	const [loading, setLoading] = useState(false);
 
-	const handleChange = (e) => {
-		const { target } = e;
-		const { name, value } = target;
+	const handleChange = (
+		e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		const { name, value } = e.target as HTMLInputElement | HTMLTextAreaElement;
 
 		setForm({
 			...form,
@@ -27,7 +28,7 @@ const Contact = () => {
 		});
 	};
 
-	const handleSubmit = (e) => {
+	const handleSubmit = (e: React.SyntheticEvent) => {
 		e.preventDefault();
 		setLoading(true);
 
